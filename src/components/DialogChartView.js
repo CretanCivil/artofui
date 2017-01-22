1fe263c070cfb8118352fb3e600c14d7c71e954d
leapar <9395462@qq.com>
2017-01-22 10:01:23 +0800

    删错了

--- src/components/DialogChartView.js
+++ src/components/DialogChartView.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Modal, } from 'antd';
 import CustomCharts from './CustomCharts';
+import { Table,Icon } from 'antd';
+import { APP_CONFIG } from './../config/app';
 
 // React.Component
 export default class DialogChartView extends React.Component {
@@ -9,6 +11,39 @@ export default class DialogChartView extends React.Component {
     }
   
     render() {
+        const data = [{
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+
+        }, {
+                key: '2',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+            }, {
+                key: '3',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+            }];
+            const columns = [{
+            title: 'Name',
+            dataIndex: 'name',
+            width: 150,
+
+            render: (value, row, index) => <Icon  type="appstore"
+                style={{ color: APP_CONFIG.colors[index], fontSize: 20 }}></Icon>
+        }, {
+                title: 'Age',
+                dataIndex: 'age',
+                width: 150,
+            }, {
+                title: 'Address',
+                dataIndex: 'address',
+                width: 150,
+            }];
         return < Modal
             className="expandCharDialog"
             title = {this.props.chart.name}
