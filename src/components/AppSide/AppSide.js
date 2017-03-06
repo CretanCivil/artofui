0f41bbda9ac7ee566ee479d8fe688d9d92ceef0c
Cary <chairui@purvar.com>
2017-03-06 11:33:03 +0800

    加菜单

--- src/components/AppSide/AppSide.js
+++ src/components/AppSide/AppSide.js
@@ -8,8 +8,8 @@ const AppSide = () => {
     <dl className="aside">
       <dt>主要</dt>
       <dd><a className="b-success active"><i><Icon type="appstore" /></i><span>平台列表</span></a></dd>
-      <dd><a className="b-event"><i><Icon type="pie-chart" /></i><span>仪表盘</span></a></dd>
-      <dd><a className="b-event"><i><Icon type="line-chart" /></i><span>事件流</span></a></dd>
+      <dd><a href="/apmsys/dashboardList" className="b-event"><i><Icon type="pie-chart" /></i><span>仪表盘</span></a></dd>
+      <dd><a href="/apmsys/eventflowList" className="b-event"><i><Icon type="line-chart" /></i><span>事件流</span></a></dd>
       <dd><a className="b-danger"><i><Icon type="notification" /></i><span>报警/预警</span></a></dd>
       <dd><a className="b-warning"><i><Icon type="pushpin-o" /></i><span>指标</span></a></dd>
       <dt>用户中心</dt>
