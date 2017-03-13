48cd576ce00a9fd0c9a08fd6472d75f9386d07cf
Cary <chairui@purvar.com>
2017-03-13 11:12:57 +0800

    菜单样式调整

--- src/components/AppSide/AppSide.js
+++ src/components/AppSide/AppSide.js
@@ -46,31 +46,34 @@ class AppSide extends Component {
   render() {
     //暂时使用a跳转，之后要用Link
     return (
-      <Menu mode="inline"
-            openKeys={this.state.openKeys}
-            selectedKeys={[this.state.current]}
-            onOpenChange={this.onOpenChange}
-            onClick={this.handleClick}
-      >
-        <MenuItemGroup title="主要">
-          <Menu.Item key="1" className="b-success"><a href="/apmsys"><i className="ion-android-apps"></i><b>平台列表</b></a></Menu.Item>
-          <SubMenu key="2" className="b-event" title={<span><i className="ion-speedometer"></i><b>仪表盘</b></span>}>
-            <Menu.Item key="21"><a href="/apmsys/dashboardList"><b>自定义仪表盘</b></a></Menu.Item>
-            <Menu.Item key="22"><a href="/apmsys/platformList"><b>平台仪表盘</b></a></Menu.Item>
-            <Menu.Item key="23"><a href="/apmsys/collectDashBoardList"><b>已收藏的仪表盘</b></a></Menu.Item>
-          </SubMenu>
-          <Menu.Item key="3" className="b-event"><a href="/apmsys/eventflowList"><i className="ion-ios-timer"></i><b>事件流</b></a></Menu.Item>
-          <Menu.Item key="4" className="b-danger"><a href="/apmsys/alarmList"><i className="ion-ios-bell"></i><b>报警/预警</b></a></Menu.Item>
-          <SubMenu key="5" className="b-warning" title={<span><i className="ion-arrow-graph-up-right"></i><b>指标</b></span>}>
-            <Menu.Item key="51"><a href="/apmsys/userMetric"><b>用户指标</b></a></Menu.Item>
-            <Menu.Item key="52"><a href="/apmsys/metrics/explore"><b>浏览</b></a></Menu.Item>
-          </SubMenu>
-        </MenuItemGroup>
-        <MenuItemGroup title="用户中心">
-          <Menu.Item key="6" className="b-default"><a href="#"><i className="ion-information"></i><b>帮助</b></a></Menu.Item>
-          <Menu.Item key="7" className="b-default"><a href="#"><i className="ion-ios-gear"></i><b>设置</b></a></Menu.Item>
-        </MenuItemGroup>
-      </Menu>
+      <div>
+        <a href="/apmsys" className="app-side-title"><img src={require('./logo.png')} alt="logo" width="32" height="32" /><span>PAAS INSIGHT</span></a>
+        <Menu mode="inline"
+              openKeys={this.state.openKeys}
+              selectedKeys={[this.state.current]}
+              onOpenChange={this.onOpenChange}
+              onClick={this.handleClick}
+        >
+          <MenuItemGroup title="主要">
+            <Menu.Item key="1" className="b-success"><a href="/apmsys"><i className="ion-android-apps"></i><b>平台列表</b></a></Menu.Item>
+            <SubMenu key="2" className="b-event" title={<span><i className="ion-speedometer"></i><b>仪表盘</b></span>}>
+              <Menu.Item key="21"><a href="/apmsys/dashboardList"><b>自定义仪表盘</b></a></Menu.Item>
+              <Menu.Item key="22"><a href="/apmsys/platformList"><b>平台仪表盘</b></a></Menu.Item>
+              <Menu.Item key="23"><a href="/apmsys/collectDashBoardList"><b>已收藏的仪表盘</b></a></Menu.Item>
+            </SubMenu>
+            <Menu.Item key="3" className="b-event"><a href="/apmsys/eventflowList"><i className="ion-ios-timer"></i><b>事件流</b></a></Menu.Item>
+            <Menu.Item key="4" className="b-danger"><a href="/apmsys/alarmList"><i className="ion-ios-bell"></i><b>报警/预警</b></a></Menu.Item>
+            <SubMenu key="5" className="b-warning" title={<span><i className="ion-arrow-graph-up-right"></i><b>指标</b></span>}>
+              <Menu.Item key="51"><a href="/apmsys/userMetric"><b>用户指标</b></a></Menu.Item>
+              <Menu.Item key="52"><a href="/apmsys/metrics/explore"><b>浏览</b></a></Menu.Item>
+            </SubMenu>
+          </MenuItemGroup>
+          <MenuItemGroup title="用户中心">
+            <Menu.Item key="6" className="b-default"><a href="#"><i className="ion-information"></i><b>帮助</b></a></Menu.Item>
+            <Menu.Item key="7" className="b-default"><a href="#"><i className="ion-ios-gear"></i><b>设置</b></a></Menu.Item>
+          </MenuItemGroup>
+        </Menu>
+      </div>
     );
   }
 }
