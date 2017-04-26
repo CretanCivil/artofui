13fc8c5a343ab2b9b40a073852474a447b380cc1
leapar <9395462@qq.com>
2017-04-26 10:07:07 +0800

    配置

--- src/config/api.js
+++ src/config/api.js
@@ -1,18 +1,24 @@
 let host;
 
-if(process.env.NODE_ENV == "test"){
+if (process.env.NODE_ENV == "test") {
   host = "http://localhost:4000";
-}else{
-  host = location.origin;
-  host = "http://172.29.231.177:8000";
+} else {
+  
+  //host = "http://172.29.231.177:8000";
+  
+  if (process.env.NODE_ENV === 'production') {
+    host = "http://" + location.hostname + ":8000";
+  } else {
+    host = location.origin;
+  }
 }
 
-let userHost = host;//"http://172.29.231.177:8000";
+let userHost = host; //"http://172.29.231.177:8000";
 
 const baseUri = host;
 console.log(G_WEB_PARAMS);
 export const API_CONFIG = {
-  apiKey: G_WEB_PARAMS.apiKey,//,'e7afaf986f5cc822406cbd5831328462',
+  apiKey: G_WEB_PARAMS.apiKey, //,'e7afaf986f5cc822406cbd5831328462',
   host: host,
   userHost: userHost,
   baseUri: baseUri,
@@ -34,17 +40,16 @@ export const API_CONFIG = {
     list: '/p1/metric_templates/list.json',
     delete: '/p1/metric_templates/{}/delete.json',
   },
-  
+
   //dashboard: '/p1/dashboards/{}/charts.json',
   show: '/p1/dashboards/{}/show.json',
   all_metrics: '/p1/metrics.json',
   //updateLayout: '/p1/dashboards/{}/update.json',
   normal_model: '/p1/metric_types/normal_mode_list',
-  metric: userHost+'/apmsys/v2/batch_query',
-  
+  metric: userHost + '/apmsys/v2/batch_query',
+
   //metric: '/v1/batch_query.json',
   tags: '/p1/tags.json',
   buckets: '/v1/events/buckets.json',
   users: '/api/v1/users',
 };
-
